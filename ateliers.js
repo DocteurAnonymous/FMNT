dc4f575c515e2e083d9ec1f2b507849231a9efd9
DocteurAnonymous <sanoismael51@gmail.com>
2025-03-28 18:44:25 +0000

    first commit

---
+++ ateliers.js
@@ -0,0 +1,111 @@
+const ateliers = [
+    {
+        id: 1,
+        title: 'Intro sur l’IA et machine learning',
+        description: 'Exploration des bases de l’intelligence artificielle et des algorithmes d’apprentissage automatique.',
+        duration: '2H',
+        trainer: "Sekou Ahmed DAFFE",
+        trainerImage: "assets/img/trainers/trainer-2.jpg",
+        image: "Images/IA&ML.jpg"
+    },
+    {
+        id: 2,
+        title: 'DAO (AutoCad)',
+        description: 'Exploration des bases de l’intelligence artificielle et des algorithmes d’apprentissage automatique.',
+        duration: '2H',
+        trainer: "Thierno Saidou SOW",
+        trainerImage: "assets/img/trainers/trainer-2.jpg",
+        image: "Images/DAO.jpg"
+    },
+    {
+        id: 3,
+        title: 'IOT (Internet Of Things)',
+        description: 'Exploration des bases de l’intelligence artificielle et des algorithmes d’apprentissage automatique.',
+        duration: '2H',
+        trainer: "Ibrahima Kalil CAMARA",
+        trainerImage: "assets/img/trainers/trainer-2.jpg",
+        image: "Images/IOT.jpg"
+    },
+    {
+        id: 4,
+        title: 'Construire un cercle social pour une bonne croissance professionnelle',
+        description: 'Exploration des bases de l’intelligence artificielle et des algorithmes d’apprentissage automatique.',
+        duration: '2H',
+        trainer: "Moustapha DIALLO",
+        trainerImage: "assets/img/trainers/trainer-2.jpg",
+        image: "Images/CSCP.jpg"
+    },
+    {
+        id: 5,
+        title: 'Gestion de base de données',
+        description: 'Exploration des bases de l’intelligence artificielle et des algorithmes d’apprentissage automatique.',
+        duration: '1H',
+        trainer: "Philippe TOLNO",
+        trainerImage: "assets/img/trainers/trainer-2.jpg",
+        image: "Images/BDD.jpg"
+    },
+    {
+        id: 6,
+        title: 'Algorithmique',
+        description: 'Exploration des bases de l’intelligence artificielle et des algorithmes d’apprentissage automatique.',
+        duration: '1H',
+        trainer: "Lama DIALLO",
+        trainerImage: "assets/img/trainers/trainer-2.jpg",
+        image: "Images/ALGO.jpg"
+    },
+    {
+        id: 7,
+        title: 'Méthode Agile (Gestion de  Projet)',
+        description: 'Exploration des bases de l’intelligence artificielle et des algorithmes d’apprentissage automatique.',
+        duration: '2H',
+        trainer: "Djibril SOUARE",
+        trainerImage: "assets/img/trainers/trainer-2.jpg",
+        image: "Images/AGILE.jpg"
+    },
+    {
+        id: 8,
+        title: 'Phishing',
+        description: 'Exploration des bases de l’intelligence artificielle et des algorithmes d’apprentissage automatique.',
+        duration: '2H',
+        trainer: "Abdoulaye DIALLO",
+        trainerImage: "assets/img/trainers/trainer-2.jpg",
+        image: "Images/PHISHING.jpg"
+    },
+    {
+        id: 9,
+        title: 'Créativité et Innovation (Techniques et méthodologies)',
+        description: 'Exploration des bases de l’intelligence artificielle et des algorithmes d’apprentissage automatique.',
+        duration: '2H',
+        trainer: "Moriba KEITA",
+        trainerImage: "assets/img/trainers/trainer-2.jpg",
+        image: "Images/C&I.jpg"
+    },
+    {
+        id: 10,
+        title: 'La conteneurisation avec Docker et Docker Compose',
+        description: 'Exploration des bases de l’intelligence artificielle et des algorithmes d’apprentissage automatique.',
+        duration: '2H',
+        trainer: "Pierre Foromo YABOIGUI",
+        trainerImage: "assets/img/trainers/trainer-2.jpg",
+        image: "Images/DOCKER.jpg"
+    },
+    {
+        id: 11,
+        title: 'Protection des données dans les entreprises',
+        description: 'Exploration des bases de l’intelligence artificielle et des algorithmes d’apprentissage automatique.',
+        duration: '1H',
+        trainer: "Alpha CONDE",
+        trainerImage: "assets/img/trainers/trainer-2.jpg",
+        image: "Images/PDDE.jpg"
+    },
+    {
+        id: 12,
+        title: 'Data processing for machine learning',
+        description: 'Exploration des bases de l’intelligence artificielle et des algorithmes d’apprentissage automatique.',
+        duration: '1H',
+        trainer: "Ansoumane CONDE",
+        trainerImage: "assets/img/trainers/trainer-2.jpg",
+        image: "Images/DATAML.jpg"
+    },
+];
+
---
+++ main.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Sélectionner l'élément qui contiendra les ateliers
+    const container = document.getElementById("ateliers-list");
+
+    // Importer les ateliers depuis le fichier ateliers.js
+    ateliers.forEach(atelier => {
+        const atelierHTML = `
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch mb-4">
+                <div class="course-item">
+                    <img src="${atelier.image}" class="img-fluid" alt="Image de ${atelier.title}">
+                    <div class="course-content">
+                        <div class="d-flex justify-content-between align-items-center mb-3">
+                            <h4>${atelier.title}</h4>
+                            <p class="price">${atelier.duration}</p>
+                        </div>
+                        <h3><a href="atelier-details.html" onclick="saveAtelier(${atelier.id})">${atelier.title}</a></h3>
+                        <p>${atelier.description}</p>
+                        <div class="trainer d-flex justify-content-between align-items-center">
+                            <div class="trainer-profile d-flex align-items-center">
+                                <img src="${atelier.trainerImage}" class="img-fluid rounded-circle" alt="Photo de ${atelier.trainer}">
+                                <span>${atelier.trainer}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        container.innerHTML += atelierHTML;
+    });
+});
+
+function saveAtelier(atelierId) {
+    const atelier = ateliers.find(a => a.id === atelierId);
+    localStorage.setItem("selectedAtelier", JSON.stringify(atelier));
+}
